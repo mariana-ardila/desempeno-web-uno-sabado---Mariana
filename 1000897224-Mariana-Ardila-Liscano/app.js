52906b4ca05118d9ce9806ad18e47951150a3a5f
mariana-ardila <desarrolladoraardila@gmail.com>
2025-09-27 14:21:45 -0500

    Fase 3: Añadida simulación de batalla y diagnóstico del héroe

--- 1000897224-Mariana-Ardila-Liscano/app.js
+++ 1000897224-Mariana-Ardila-Liscano/app.js
@@ -31,3 +31,49 @@ const ganarExperiencia=(cantidadExp)=>{
 }
 recibirDanio(40);
 ganarExperiencia(100)
+
+
+//Punto # 3
+
+
+const simularBatalla=()=>{
+for(let i=0; i<=2; i++){
+recibirDanio(50);
+}
+if (puntoSalud>0){
+    ganarExperiencia(100)
+}
+}
+
+ console.log("puntos experiencia " + puntosExperiencia)
+
+
+function diagnosticoHeroe(){
+    if(puntoSalud>=100){
+    console.log(`${nombreHeroe} tu salud es excelente`)
+    }
+    else if(puntoSalud>=40){
+        console.log(`${nombreHeroe} considera usar una poción`)
+    }
+    else if(puntoSalud>0){
+                console.log(`¡${nombreHeroe} peligro! Salud crítica`)
+    }
+
+    switch(puntosExperiencia){
+    case 100:
+        console.log(`${nombreHeroe} eres una leyenda viviente`)
+    break;
+    case 200:
+        console.log(`${nombreHeroe} Te estás convirtiendo en un guerrero respetable`)
+    break;
+    case 20:
+        console.log(`${nombreHeroe} Eres un novato`)
+    break;
+        default:
+            console.log("Valor no válido. ")
+    }
+   
+}
+simularBatalla();
+diagnosticoHeroe();
+
